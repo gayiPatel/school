4a58ca8f4faf76caf88dc944c01fb2b3f06fd1ab
Gayatri Patel <gayatrimscit@gmail.com>
2023-05-03 12:58:37 +0530

    added code for environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:8080/api/v1',
+  production: true,
+  apiBaseUrl: 'http://ec2-52-63-120-208.ap-southeast-2.compute.amazonaws.com:8080/api/v1',
 };
